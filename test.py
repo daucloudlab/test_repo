1c0beaf7ce89bc339b510cdb6bf89446cd4bc168
Daulet <daucloudlab@gmail.com>
2021-10-30 20:58:53 +0600

    test.py is changed and .gitignore is changed

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ print("Hello World!")
 a = "Hello World"
 s = a.upper()
 l = s.split()
-s = " ".join()
-print(s)
 s = "1ab 2345 "
 s = s.rstrip()
 print(s)
+a = map(lambda x : x.upper, a)
+a = list(a)
+a = "".join(a)
+print(a)
