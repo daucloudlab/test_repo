4eec2e9e4bee63e686ac65f4ffca31c23742d4de
Daulet <daucloudlab@gmail.com>
2021-11-30 10:54:42 +0600

    before pushing

--- welcome.py
+++ welcome.py
@@ -1,3 +1,21 @@
 print("salem!")
 print("from testing branch")
 print("git diff command")
+
+def test(num):
+    //dsmdkms
+    return ?
+
+a = test(1236)
+a == 6
+
+
+def __init__(self, id = 0, firsName = "", lastName = ""):
+        self.id = id
+        self.firstName = firsName
+        self.lastName = lastName
+
+Employee(1,"sds", "sdd")
+
+r1 = Rectangle(2,4)
+r2 = Rectangle(1,4)
